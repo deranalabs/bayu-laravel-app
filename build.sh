f559ebfe58549aa5e4d09374a88b5a40549c0f0d
Bayumlyn <113621505+deranalabs@users.noreply.github.com>
2025-06-27 17:45:34 +0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -o errexit
+
+composer install --no-dev --optimize-autoloader
+php artisan key:generate
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
